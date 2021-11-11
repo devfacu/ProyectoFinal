1f03870ab4109848c71fbf170994413ae59d01df
Mariano Prado <nanoprado3@gmail.com>
2021-11-10 21:10:19 -0300

    master commit

---
+++ src/test/java/com/deggvelopers/pomodoro/PomodoroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deggvelopers.pomodoro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomodoroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
