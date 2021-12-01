952dd6710d1fd5649012c836f9a69c443abdd030
Mariano Prado <nanoprado3@gmail.com>
2021-11-30 22:16:40 -0300

    Corrio el programa

--- src/main/java/com/deggvelopers/pomodoro/configuracion/Login.java
+++ src/main/java/com/deggvelopers/pomodoro/configuracion/Login.java
@@ -3,40 +3,45 @@ package com.deggvelopers.pomodoro.configuracion;
 import com.deggvelopers.pomodoro.servicio.UsuarioServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class Login extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UsuarioServicio usuarioServicio;
 
-//    @Autowired
-//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//        auth
-//                .userDetailsService((T) usuarioServicio)
-//                .passwordEncoder(new BCryptPasswordEncoder());
-//    }
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(usuarioServicio)
+                .passwordEncoder(new BCryptPasswordEncoder());
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-            http.authorizeRequests().antMatchers("/css/*", "/js/*", "/img/*").permitAll().anyRequest().authenticated()
-                    
+        http
+                .authorizeRequests()
+                .antMatchers("/css/", "/js/", "/img/", "/audio/", "/registrar").permitAll()
                 .and()
-                    .formLogin().loginPage("/").defaultSuccessUrl("/usuario/inicio",true)
-                .loginProcessingUrl("")
-                .usernameParameter("mail")
-                .passwordParameter("password")              
+                .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/logincheck")
+                .usernameParameter("email")
+                .passwordParameter("contrasena")
+                .defaultSuccessUrl("/principal")
                 .permitAll()
-                .and().logout()
-                .logoutUrl("f")
+                .and()
+                .logout()
+                .logoutUrl("/logout")
                 .logoutSuccessUrl("/")
                 .permitAll();
     }
--- src/main/java/com/deggvelopers/pomodoro/servicio/UsuarioServicio.java
+++ src/main/java/com/deggvelopers/pomodoro/servicio/UsuarioServicio.java
@@ -1,6 +1,5 @@
 package com.deggvelopers.pomodoro.servicio;
 
-import com.deggvelopers.pomodoro.entidad.Proyecto;
 import com.deggvelopers.pomodoro.entidad.Usuario;
 import com.deggvelopers.pomodoro.repositorio.UsuarioRepositorio;
 import java.util.ArrayList;
@@ -8,53 +7,52 @@ import java.util.List;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
 @Service
 @Validated
-public class UsuarioServicio  {
+public class UsuarioServicio implements UserDetailsService {
 
     @Autowired
     private UsuarioRepositorio usuarioRepositorio;
 
     @Transactional
-    public Usuario registrarUsuario(@Validated String nombre, String apellido, String mail, String password) throws Exception {
+    public Usuario registrar(@Validated String nombre, String apellido, String mail, String password) throws Exception {
         Usuario usuario = new Usuario();
+
+        if (nombre == null || nombre.isEmpty() && !nombre.matches("^[a-zA-Z]*$")) {
+            throw new Exception("El nombre no puede estar vacio.");
+        }
+
+        if (apellido == null || apellido.isEmpty() && !apellido.matches("^[a-zA-Z]*$")) {
+            throw new Exception("El apellido no puede estar vacio.");
+        }
+
+        if (mail == null || mail.isEmpty() && !mail.matches("^[a-zA-Z]*$")) {
+            throw new Exception("Debe ingresar un mail valido.");
+        }
+
+        if (password == null || password.isEmpty() && !password.matches("^[a-zA-Z]*$")) {
+            throw new Exception("El nombre no puede estar vacio.");
+        }
 
         usuario.setNombre(nombre);
         usuario.setApellido(apellido);
         usuario.setMail(mail);
         usuario.setPassword(password);
 
-        if (nombre == null || nombre.isEmpty() && !nombre.matches("^[a-zA-Z]*$")) {
-            throw new Exception("Al registrarse le falto completar algun campo");
-        }
-
-        if (apellido == null || nombre.isEmpty() && !apellido.matches("^[a-zA-Z]*$")) {
-            throw new Exception("Al registrarse le falto completar algun campo");
-        }
-
-        if (mail == null || nombre.isEmpty() && !mail.matches("^[a-zA-Z]*$")) {
-            throw new Exception("Al registrarse le falto completar algun campo");
-        }
-
-        if (password == null || nombre.isEmpty() && !password.matches("^[a-zA-Z]*$")) {
-            throw new Exception("Al registrarse le falto completar algun campo");
-        }
-
 //        Proyecto proyecto = new Proyecto();
 //
 //        proyecto.setNombre(nombre);
 //        proyecto.setUsuario(usuario);
-
         return usuario;
     }
-    
- 
 
 //    public Proyecto nuevoProyecto (Usuario usuario) {
 //        Proyecto proyecto = new Proyecto();
@@ -64,8 +62,28 @@ public class UsuarioServicio  {
 //
 //        return proyecto;
 //    }
-
     public static boolean validacion(String datos) {
         return datos.matches("a-zA-Z*");
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepositorio.buscarPorMail(email);
+        if (usuario != null) {
+
+            List<GrantedAuthority> permisos = new ArrayList<>();
+
+            GrantedAuthority p1 = new SimpleGrantedAuthority("ROLE_USUARIO_REGISTRADO");
+            permisos.add(p1);
+
+//			ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+//			HttpSession session = attr.getRequest().getSession(true);
+//			session.setAttribute("usuarioSession", usuario);
+            User user = new User(usuario.getMail(), usuario.getPassword(), permisos);
+            return user;
+        } else {
+            return null;
+        }
+
     }
 }
