d899be0c57b9da71762e7fbfc72ae13bf60a4e47
Adrian <adrianpicardi989@gmail.com>
2021-12-02 20:03:51 -0300

    elimina boostrap.min, se agrega usuario modificado por facu, styles actualizado, registro actualizada

--- src/main/java/com/deggvelopers/pomodoro/entidad/Usuario.java
+++ src/main/java/com/deggvelopers/pomodoro/entidad/Usuario.java
@@ -25,8 +25,8 @@ public class Usuario implements Serializable {
 	private String mail;
 	private String password;
 	private Boolean habilitado;
-//	@OneToOne
-//	private Configuracion configuracion;
+	@OneToOne
+	private Configuracion configuracion;
 
 	///constructor vacio
 	public Usuario() {
@@ -90,4 +90,5 @@ public class Usuario implements Serializable {
 		this.habilitado = habilitado;
 	}
 
+}
 }
