b65892403d850e5c7ea73f220edb2c7f29cc860e
Lisbeth <programacionlisbethbetancourt@gmail.com>
2021-12-02 21:35:08 -0300

    creacion clase enum prioridad

---
+++ src/main/java/com/deggvelopers/pomodoro/entidad/Prioridad.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.deggvelopers.pomodoro.entidad;
+
+/**
+ *
+ * @author Administrador
+ */
+public enum Prioridad {
+    
+}
